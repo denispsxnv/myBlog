93c220d0eb72204b3951bd9c8e4835d154e7fe2e
denispsxnv <denisazizbekov@cloud.com>
2023-04-19 03:11:39 +0600

    footer

--- src/componets/Footer/Footer.jsx
+++ src/componets/Footer/Footer.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from "./footer.module.css"
+import inImg from "./footer-img1.png"
+import footerImg from "./footer-img3.png"
+import gitImg from "./git-img2.png"
+import youTubeImg from "./footer-img4.png"
+import twitterImg from "./tfitter-img5.png"
 
 const Footer = () => {
     return (
-        <footer>
-             <p>Get in touch with me on other platforms</p>
-             <Link href="#" className={styles.link}>link</Link>
-             <Link href="#" className={styles.link}></Link>
-             <Link href="#" className={styles.link}></Link>
-             <Link href="#" className={styles.link}></Link>
-             <Link href="#" className={styles.link}></Link>
-             <Link href="#" className={styles.link}></Link>
+        <footer className={styles.footer}>
+             <p className={styles.title}>Get in touch with me on other platforms</p>
+             <Link href="#" className={styles.link}><img src={inImg} alt="" /></Link>
+             <Link href="#" className={styles.link}><img src={gitImg} alt="" /></Link>
+             <Link href="#" className={styles.link}><img src={footerImg} alt="" /></Link>
+             <Link href="#" className={styles.link}><img src={youTubeImg} alt="" /></Link>
+             <Link href="#" className={styles.link}><img src={twitterImg} alt="" /></Link>
         </footer>
     );
 };
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -2,80 +2,92 @@ import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import styles from './registerPage.module.css'
-import { authServices } from '../../Services/auth';
+import { useFormik } from 'formik';
+import { authServices } from '../../Services/auth'
+import 'react-toastify/dist/ReactToastify.css'
+import { toast, ToastContainer } from 'react-toastify';
 
 const RegisterPage = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [name, setName] = useState('')
 
-    const { registration } = authServices()
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            username: ''
+        },
+        onSubmit: async (values) => {
+            try {
+                const data = await authServices.register(values)
+                console.log(data);
+                toast('пользователь зареган')
+            } catch (err) {
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const newUser = {
-            username : name,
-            email,
-            password
-            
+                toast(err.response.data)
+            }
         }
-        try {
-        const { data } = await registration(newUser)
-        console.log(data)
-        } catch (err) {
-            console.log(err.respons.data)
-        }
-        // registration(newUser)
-    }
+    }) 
+ 
 
-    return (
-        <section className={styles.wrapper}>
-            <h1 className={styles.title}>
-                Регистрация
-            </h1>
-            <form className={styles.form} onSubmit={handleSubmit}>
-                <TextField
-                    label="Ваш @mail"
-                    variant="filled"
-                    type="email"
-                    fullWidth
-                    requared
-                    style={{ marginBottom: "20px" }}
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                />
-                <TextField
-                    label="Ваше имя"
-                    variant="filled"
-                    type="text"
-                    fullWidth
-                    requared
-                    style={{ marginBottom: "20px" }}
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                />
-                <TextField
-                    label="Ваш Пароль"
-                    variant="filled"
-                    type="password"
-                    fullWidth
-                    requared
-                    style={{ marginBottom: "40px" }}
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                />
-                <Button
-                    type="submit"
-                    variant="contained"
-                    fullWidth
-                    style={{
-                        backgroundColor: "#BF94E8",
-                        fontSize: "24px"
-                    }}
-                >Зарегистрироваться</Button>
-            </form>
-        </section>
-    );
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
+    //     const newUser = {
+    //         email,
+    //         name,
+    //         password
+    //     }
+    //     console.log(newUser)
+    // }
+return (
+    <section className={styles.wrapper}>
+        <h1 className={styles.title}>
+            Регистрация
+        </h1>
+        <form className={styles.form} onSubmit={formik.handleSubmit}>
+            <TextField
+                label="Ваш @mail"
+                variant="filled"
+                type="email"
+                name='email'
+                fullWidth
+                required
+                style={{ marginBottom: "20px" }}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+
+            />
+            <TextField
+                label="Ваше имя"
+                variant="filled"
+                type="text"
+                name='username'
+                fullWidth
+                required
+                style={{ marginBottom: "20px" }}
+                value={formik.values.username}
+                onChange={formik.handleChange}
+            />
+            <TextField
+                label="Ваш Пароль"
+                variant="filled"
+                type="password"
+                name='password'
+                fullWidth
+                required
+                style={{ marginBottom: "40px" }}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+
+            />
+            <Button
+                type="submit"
+                variant="contained"
+                fullWidth
+                style={{ backgroundColor: "#BF94E8", fontSize: "24px" }}
+            >Зарегистрироваться</Button>
+            <ToastContainer/>
+        </form>
+    </section>
+);
 };
 
 export default RegisterPage;
